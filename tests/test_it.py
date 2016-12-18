8bc6d3ee07736d4e5f2c562a5376762a121f504f
Michael Merickel <michael@merickel.org>
2016-12-18 01:10:36 -0600

    delay the integration tests

--- tests/test_it.py
+++ tests/test_it.py
@@ -9,6 +9,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 def test_myapp_reloads_when_touching_ini(testapp):
     testapp.start('myapp', ['--reload'])
     testapp.wait_for_response(interval=1)
+    time.sleep(1)
     util.touch(os.path.join(here, 'myapp/foo.ini'))
     testapp.wait_for_response()
     testapp.stop()
@@ -21,6 +22,7 @@ def test_myapp_reloads_when_touching_ini(testapp):
 def test_myapp_reloads_when_touching_pyfile(testapp):
     testapp.start('myapp', ['--reload'])
     testapp.wait_for_response(interval=1)
+    time.sleep(1)
     util.touch(os.path.join(here, 'myapp/cli.py'))
     testapp.wait_for_response()
     testapp.stop()
