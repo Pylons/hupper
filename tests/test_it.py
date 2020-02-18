f4d99c6029c4ad493c54d9a49d0ffc27badd65b9
Bert JW Regeer <bertjw@regeer.org>
2020-02-18 00:48:09 -0800

    Revert "bump wait"
    This reverts commit fbf8ab4bcb8871c7799165808ef241ed47ab076a.

--- tests/test_it.py
+++ tests/test_it.py
@@ -9,7 +9,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 def test_myapp_reloads_when_touching_ini(testapp):
     testapp.start('myapp', ['--reload'])
     testapp.wait_for_response()
-    time.sleep(5)
+    time.sleep(2)
     util.touch(os.path.join(here, 'myapp/foo.ini'))
     testapp.wait_for_response()
     testapp.stop()
@@ -21,7 +21,7 @@ def test_myapp_reloads_when_touching_ini(testapp):
 def test_myapp_reloads_when_touching_pyfile(testapp):
     testapp.start('myapp', ['--reload'])
     testapp.wait_for_response()
-    time.sleep(5)
+    time.sleep(2)
     util.touch(os.path.join(here, 'myapp/cli.py'))
     testapp.wait_for_response()
     testapp.stop()
