fbf8ab4bcb8871c7799165808ef241ed47ab076a
Michael Merickel <michael@merickel.org>
2020-02-17 23:18:44 -0600

    bump wait

--- tests/test_it.py
+++ tests/test_it.py
@@ -9,7 +9,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 def test_myapp_reloads_when_touching_ini(testapp):
     testapp.start('myapp', ['--reload'])
     testapp.wait_for_response()
-    time.sleep(2)
+    time.sleep(5)
     util.touch(os.path.join(here, 'myapp/foo.ini'))
     testapp.wait_for_response()
     testapp.stop()
@@ -21,7 +21,7 @@ def test_myapp_reloads_when_touching_ini(testapp):
 def test_myapp_reloads_when_touching_pyfile(testapp):
     testapp.start('myapp', ['--reload'])
     testapp.wait_for_response()
-    time.sleep(2)
+    time.sleep(5)
     util.touch(os.path.join(here, 'myapp/cli.py'))
     testapp.wait_for_response()
     testapp.stop()
