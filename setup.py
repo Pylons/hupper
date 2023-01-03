c82b51c6190972b4a05b0c88198fab2c8cd13ea6
Michael Merickel <michael@merickel.org>
2023-01-02 20:54:47 -0600

    prep 1.11

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ tests_require = ['watchdog', 'pytest', 'pytest-cov', 'mock']
 
 setup(
     name='hupper',
-    version='1.10.3',
+    version='1.11',
     description=(
         'Integrated process monitor for developing and reloading daemons.'
     ),
