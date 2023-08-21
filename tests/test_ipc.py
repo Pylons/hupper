e13d393783a08f0799596c292092fb5fa95dcf04
Thomas Grainger <tagrain@gmail.com>
2023-08-21 21:06:49 +0100

    only activate one end of pipe per process
    windows serializes all access to pipes so a close will block until a
    read returns
    https://groups.google.com/g/microsoft.public.win32.programmer.kernel/c/VzaZ41mDcVs/m/zSshHYVXyvQJ

--- tests/test_ipc.py
+++ tests/test_ipc.py
@@ -1,21 +1,36 @@
 import queue
 
-from hupper.ipc import Pipe
+from hupper.ipc import Pipe, spawn
+
+
+def echo(pipe):
+    q = queue.Queue()
+    pipe.activate(q.put)
+    msg = q.get()
+    while msg is not None:
+        pipe.send(msg)
+        msg = q.get()
+    pipe.close()
+    pipe.reader_thread.join()
 
 
 def test_ipc_close():
     c1, c2 = Pipe()
     c1_q = queue.Queue()
-    c2_q = queue.Queue()
     c1.activate(c1_q.put)
-    c2.activate(c2_q.put)
 
-    c1.send("hello")
-    c2.send("world")
-    assert c2_q.get() == "hello"
-    assert c1_q.get() == "world"
+    with spawn(
+        __name__ + '.echo',
+        kwargs={"pipe": c2},
+        pass_fds=[c2.r_fd, c2.w_fd],
+    ) as proc:
+        try:
+            c2.close()
 
-    c1.close()
-    c2.close()
-    c1.reader_thread.join()
-    c2.reader_thread.join()
+            c1.send("hello world")
+            assert c1_q.get() == "hello world"
+
+            c1.close()
+            c1.reader_thread.join()
+        finally:
+            proc.terminate()
